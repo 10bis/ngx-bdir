6a5aaac09b1f11b8e478f65d30941e58861ded1a
Denis Levkov <denis@500tech.com>
2019-06-11 18:15:42 +0300

    test: 💍 gained full coverage

--- projects/ngx-bdir/src/lib/b-dir.service.spec.ts
+++ projects/ngx-bdir/src/lib/b-dir.service.spec.ts
@@ -5,9 +5,34 @@ describe('BDirService', () => {
   let service: BDirService;
   beforeEach(() => {
     service = new BDirService(['he']);
+    expect.hasAssertions();
   });
   it('can load instance', () => {
     expect(service).toBeTruthy();
+  });
+  describe('should check ', () => {
+    it('should set current dir to "undefined" on given dir "undefined"', done => {
+      service.setDir(undefined);
+      service.getDir$().subscribe(x => {
+        expect(x).toBeUndefined();
+        done();
+      });
+    });
+    it('should set current dir to "LTR" on given dir "LTR"', done => {
+      service.setDir(Direction.Ltr);
+      service.setDir(Direction.Rtl);
+      service.getDir$().subscribe(x => {
+        expect(x).toBe(Direction.Rtl);
+        done();
+      });
+    });
+    it('should set current dir to "undefined" on given dir "undefined"', done => {
+      service.setDir(Direction.Rtl);
+      service.getDir$().subscribe(x => {
+        expect(x).toBeDefined();
+        done();
+      });
+    });
   });
   describe('setLang should call setDir', () => {
     it('makes expected calls', () => {
@@ -17,10 +42,19 @@ describe('BDirService', () => {
     });
   });
   describe('getOppositeDir$ should call getDir$', () => {
-    it('makes expected calls', () => {
-      const spy = spyOn(service, 'getDir$').and.returnValue(of(Direction.Ltr));
-      service.getOppositeDir$().subscribe();
-      expect(spy).toHaveBeenCalled();
+    it('should return "RTL" on given "LTR"', done => {
+      spyOn(service, 'getDir$').and.returnValue(of(Direction.Ltr));
+      service.getOppositeDir$().subscribe(x => {
+        expect(x).toBe(Direction.Rtl);
+        done();
+      });
+    });
+    it('should return "LTR" on given "RTL"', done => {
+      spyOn(service, 'getDir$').and.returnValue(of(Direction.Rtl));
+      service.getOppositeDir$().subscribe(x => {
+        expect(x).toBe(Direction.Ltr);
+        done();
+      });
     });
   });
 });
